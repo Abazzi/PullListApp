3fa756e105e9075a3f0ebf4318242b8719bec5a6
Adam Bazzi <adamjamil.bazzi64@stclairconnect.ca>
2019-11-01 00:18:09 -0400

    created detail view and matching class

---
+++ PullListApp/ComicDetailViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ComicDetailViewController.swift
+//  PullListApp
+//
+//  Created by Adam Bazzi on 2019-11-01.
+//  Copyright © 2019 Adam Bazzi. All rights reserved.
+//
+
+import UIKit
+
+class ComicDetailViewController: UIViewController {
+    
+    var comic: Comic?
+    @IBOutlet weak var cover: UIImageView!
+    
+    @IBOutlet weak var comicTitle: UILabel!
+    @IBOutlet weak var creators: UILabel!
+    @IBOutlet weak var publisher: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var diamondID: UILabel!
+
+    @IBOutlet weak var comicDescription: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        if let comic = comic{
+            comicTitle.text = comic.title
+            creators.text = comic.creators
+            publisher.text = comic.publisher
+            price.text = comic.price
+            releaseDate.text = comic.releaseDate
+            diamondID.text = comic.diamondID
+            
+            comicDescription.text = comic.description
+            
+        }
+        
+        let imageURL = URL(string: "https://dak9jkjr5v1f7.cloudfront.net/images/\(diamondID).thumbnail.png")!
+               
+               let getImageTask = URLSession.shared.downloadTask(with: imageURL, completionHandler: {
+                   url, responce, error in
+                   if error == nil, let url = url, let data = try? Data(contentsOf: url), let image = UIImage(data: data){
+                       DispatchQueue.main.async {
+                           self.cover?.image = image
+                       }
+                   }
+               })
+               
+               getImageTask.resume()
+    }
+        // Do any additional setup after loading the view.
+}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
--- PullListApp/FullListViewController.swift
+++ PullListApp/FullListViewController.swift
@@ -9,14 +9,37 @@
 import UIKit
 
 class FullListViewController: UIViewController {
+    
+    var results: [Comic] = []
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        searchBar.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
+    func createComicApiURL(searchBarText: String) -> URL {
+        
+        //delete any non friendly characters
+        
+        guard let cleanedString = searchBarText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else{
+            fatalError("Can not create appropriate URL")
+        }
+        
+        //only gets titles
+        let urlString = "https://api.shortboxed.com/comics/v1/query?title=\(cleanedString)"
+        
+        let url = URL(string: urlString)
+        return url!
+    }
+    
+    
     /*
     // MARK: - Navigation
 
@@ -27,4 +50,102 @@ class FullListViewController: UIViewController {
     }
     */
 
+}
+
+extension FullListViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text,
+        !searchText.isEmpty else {return}
+        
+        results = []
+        
+        let url = createComicApiURL(searchBarText: searchText)
+        
+        print(url)
+        
+        let dataTask = URLSession.shared.dataTask(with: url){
+            data, responce, error in
+            
+            if let error = error {
+                print("There was an error getting the data -\(error)")
+            }else{
+                
+                do{
+                    guard let data = data else {return}
+                    
+                    let decoder = JSONDecoder()
+                    
+                    let downloadedResults = try decoder.decode(Comics.self, from: data)
+                    
+                    self.results = downloadedResults.results
+                    
+                } catch let error{
+                    print(error)
+                }
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        dataTask.resume()
+        
+        searchBar.text = ""
+        
+        searchBar.resignFirstResponder()
+       }
+   }
+
+extension FullListViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+        
+}
+
+extension FullListViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ComicListing", for: indexPath)
+        
+        let comic = results[indexPath.row]
+        
+        cell.textLabel?.text = comic.title
+        cell.detailTextLabel?.text = comic.creators
+
+        let imageURL = URL(string: "https://dak9jkjr5v1f7.cloudfront.net/images/\(comic.diamondID).thumbnail.png")!
+        
+        let getImageTask = URLSession.shared.downloadTask(with: imageURL, completionHandler: {
+            url, responce, error in
+            if error == nil, let url = url, let data = try? Data(contentsOf: url), let image = UIImage(data: data){
+                DispatchQueue.main.async {
+                    cell.imageView?.image = image
+                }
+            }
+        })
+        
+        getImageTask.resume()
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "showDetails":
+            guard let indexPath = tableView.indexPathForSelectedRow else {return}
+            
+            let comic = results[indexPath.row]
+            
+            let vc = segue.destination as! ComicDetailViewController
+            
+            vc.comic = comic
+        default:
+            return
+        }
+    }
+    
+    
+    
 }
